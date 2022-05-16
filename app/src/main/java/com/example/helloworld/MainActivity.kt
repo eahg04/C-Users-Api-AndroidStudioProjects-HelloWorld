5155c5b0e851705e8f32bb3b162c396218deaea7
eahg04 <arleyherrera@gmail.com>
2022-05-16 09:43:58 -0500

    primeros componentes basicos

---
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.helloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import kotlin.math.log
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val email = findViewById<EditText>(R.id.email)
+        val sendButton = findViewById<Button>(R.id.send)
+
+        sendButton.setOnClickListener{
+            val message = "Email : ${email.text}"
+            Toast.makeText(this, message,Toast.LENGTH_SHORT).show()
+        }
+    }
+}
