f4c31d2898d73a59dd115932cec9797b5d297383
eahg04 <arleyherrera@gmail.com>
2022-05-17 14:56:44 -0500

    add basic components

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,26 +1,39 @@
 package com.example.helloworld
 
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import android.widget.Toast
-import kotlin.math.log
+import android.widget.*
+import java.util.prefs.AbstractPreferences
 
 class MainActivity : AppCompatActivity() {
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val email = findViewById<EditText>(R.id.email)
+        val spinner = findViewById<Spinner>(R.id.idspnElementos)
+
+        val lista =
+            listOf("Lenguaje de Preferencia", "JavaScript", "Java", "Kotlin", "Phyton", "HTML")
+
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, lista)
+        spinner.adapter = adaptador
+
+
+        val name = findViewById<EditText>(R.id.etName)
+        val telephone = findViewById<EditText>(R.id.phone)
         val sendButton = findViewById<Button>(R.id.send)
 
-        sendButton.setOnClickListener{
-            val message = "Email : ${email.text}"
-            Toast.makeText(this, message,Toast.LENGTH_SHORT).show()
+        sendButton.setOnClickListener {
+            val message = "Nombre : ${name.text}, Telefono : ${telephone.text}"
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+
         }
+
     }
 }
+
+
+
